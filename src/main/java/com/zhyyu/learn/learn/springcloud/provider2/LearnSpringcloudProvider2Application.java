b5e6a2339ba5aefd87cef4a3da8b897bab15ab43
juror.yu <juror.yu@mart.blue>
2019-08-19 09:57:48 +0800

    init

---
+++ src/main/java/com/zhyyu/learn/learn/springcloud/provider2/LearnSpringcloudProvider2Application.java
@@ -0,0 +1,13 @@
+package com.zhyyu.learn.learn.springcloud.provider2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringcloudProvider2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringcloudProvider2Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/zhyyu/learn/learn/springcloud/provider2/controller/Provider2Controller.java
@@ -0,0 +1,21 @@
+package com.zhyyu.learn.learn.springcloud.provider2.controller;
+
+import com.zhyyu.learn.learn.springcloud.provider.api.dto.MyDTO1;
+import com.zhyyu.learn.learn.springcloud.provider.api.service.FeignApi2Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author juror
+ * @datatime 2019/7/18 17:46
+ */
+@RestController
+@RequestMapping("provider2")
+public class Provider2Controller implements FeignApi2Service {
+
+    @Override
+    public String helloFromFeignApi2(MyDTO1 myDTO1) {
+        return myDTO1.toString();
+    }
+
+}
